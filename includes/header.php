7dfac8c8a093f760ce8ff46288c4fa6e4442d917
Saurabh <sp1141997@gmail.com>
2021-09-18 11:58:03 +0530

    some styling

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,4 @@
-
-<?php 
+<?php
 // include 'db.php';
 session_start();
 if (!isset($_SESSION['merchant_id'])) {
@@ -27,7 +26,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 
 
-  
+
 $url = "https://3-upstesting.site/delta_api/index.php/web/Login/get_merchant";
 
 $curl = curl_init($url);
@@ -35,8 +34,8 @@ curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 $headers = array(
-   "Accept: application/json",
-   "Authorization1:".$token
+    "Accept: application/json",
+    "Authorization1:" . $token
 );
 
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); //for debug only!
@@ -53,7 +52,7 @@ $merchant_data = $resp->data[0];
 // print_r($res->data[0]);
 
 // echo "<script>alert($merchant_data->merchant_id)</script>"
-  
+
 ?>
 
 <!-- fontawesome -->
@@ -67,27 +66,28 @@ $merchant_data = $resp->data[0];
 <link rel="stylesheet" href="assets/css/components.min.css">
 
 </head>
-<body class="layout-4">
-<!-- Page Loader -->
-<div class="page-loader-wrapper">
-    <span class="loader"><span class="loader-inner"></span></span>
-</div>
 
-<div id="app">
-    <div class="main-wrapper main-wrapper-1">
-<div class="navbar-bg"></div>
-        
-        <!-- Start app top navbar -->
-        <nav class="navbar navbar-expand-lg main-navbar">
-            <form class="form-inline mr-auto">
-                <ul class="navbar-nav mr-3">
-                    <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
-                    <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
-                </ul>
-                
-            </form>
-            <ul class="navbar-nav navbar-right">
-                <!-- <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
+<body class="layout-4">
+    <!-- Page Loader -->
+    <div class="page-loader-wrapper">
+        <span class="loader"><span class="loader-inner"></span></span>
+    </div>
+
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg"></div>
+
+            <!-- Start app top navbar -->
+            <nav class="navbar navbar-expand-lg main-navbar">
+                <form class="form-inline mr-auto">
+                    <ul class="navbar-nav mr-3">
+                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+                        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+                    </ul>
+
+                </form>
+                <ul class="navbar-nav navbar-right">
+                    <!-- <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
                     <div class="dropdown-menu dropdown-list dropdown-menu-right">
                     <div class="dropdown-header">Notifications
                         <div class="float-right">
@@ -145,62 +145,44 @@ $merchant_data = $resp->data[0];
                     </div>
                     </div>
                 </li> -->
-                <li class="dropdown">
-                    <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                    <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-
-                    <div class="d-sm-none d-lg-inline-block">Hi, <?php echo $merchant_data->merchant_name ?></div></a>
-
-                    <div class="d-sm-none d-lg-inline-block">Hi, Merchant</div></a>
-
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <div class="dropdown-title">Logged In</div>
-                        <a href="profile.php" class="dropdown-item has-icon"><i class="far fa-user"></i> Profile</a>
-                        <div class="dropdown-divider"></div>
-                        <a href="./backend/script.php?type=logout" class="dropdown-item has-icon text-danger"><i class="fas fa-sign-out-alt"></i> Logout</a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-
-        <!-- Start main left sidebar menu -->
-        <div class="main-sidebar sidebar-style-3">
-            <aside id="sidebar-wrapper">
-                <div class="sidebar-brand">
-                    <a href="index-2.html">3UPS</a>
-                </div>
-                <div class="sidebar-brand sidebar-brand-sm">
-                    <a href="index-2.html">3UPS</a>
-                </div>
-                <ul class="sidebar-menu">
-                    <li class="menu-header">Dashboard</li>
-                    <li class="dropdown active"><a href="index.php" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
-                    <!-- <li class="menu-header">Starter</li>
                     <li class="dropdown">
-                        <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
-                        <ul class="dropdown-menu">
-                            <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
-                            <li><a class="nav-link" href="bootstrap-badge.html">Badge</a></li>
-                            <li><a class="nav-link" href="bootstrap-breadcrumb.html">Breadcrumb</a></li>
-                            <li><a class="nav-link" href="bootstrap-buttons.html">Buttons</a></li>
-                            <li><a class="nav-link" href="bootstrap-card.html">Card</a></li>
-                            <li><a class="nav-link" href="bootstrap-carousel.html">Carousel</a></li>
-                            <li><a class="nav-link" href="bootstrap-collapse.html">Collapse</a></li>
-                            <li><a class="nav-link" href="bootstrap-dropdown.html">Dropdown</a></li>
-                            <li><a class="nav-link" href="bootstrap-form.html">Form</a></li>
-                            <li><a class="nav-link" href="bootstrap-list-group.html">List Group</a></li>
-                            <li><a class="nav-link" href="bootstrap-media-object.html">Media Object</a></li>
-                            <li><a class="nav-link" href="bootstrap-modal.html">Modal</a></li>
-                            <li><a class="nav-link" href="bootstrap-nav.html">Nav</a></li>
-                            <li><a class="nav-link" href="bootstrap-navbar.html">Navbar</a></li>
-                            <li><a class="nav-link" href="bootstrap-pagination.html">Pagination</a></li>
-                            <li><a class="nav-link" href="bootstrap-popover.html">Popover</a></li>
-                            <li><a class="nav-link" href="bootstrap-progress.html">Progress</a></li>
-                            <li><a class="nav-link" href="bootstrap-table.html">Table</a></li>
-                            <li><a class="nav-link" href="bootstrap-tooltip.html">Tooltip</a></li>
-                            <li><a class="nav-link" href="bootstrap-typography.html">Typography</a></li>
-                        </ul>
-                    </li> -->
+                        <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                            <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+
+                            <div class="d-sm-none d-lg-inline-block">Hi, <?php echo $merchant_data->merchant_name ?></div>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <div class="dropdown-title">Logged In</div>
+                            <a href="profile.php" class="dropdown-item has-icon"><i class="far fa-user"></i> Profile</a>
+                            <div class="dropdown-divider"></div>
+                            <a href="./backend/script.php?type=logout" class="dropdown-item has-icon text-danger"><i class="fas fa-sign-out-alt"></i> Logout</a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+
+            <!-- Start main left sidebar menu -->
+            <div class="main-sidebar sidebar-style-3">
+                <aside id="sidebar-wrapper">
+                    <div class="sidebar-brand">
+                        <a href="index-2.html">3UPS</a>
+                    </div>
+                    <div class="sidebar-brand sidebar-brand-sm">
+                        <a href="index-2.html">3UPS</a>
+                    </div>
+                    <ul class="sidebar-menu">
+                        <li class="menu-header">Dashboard</li>
+                        <li class="dropdown active"><a href="index.php" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a></li>
+                        <li class="menu-header">Starter</li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Products</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="#">Add Banner</a></li>
+                                <li><a class="nav-link" href="#">Add Category</a></li>
+                                <li><a class="nav-link" href="#">Add Sub-Category</a></li>
+                                <li><a class="nav-link" href="#">Add Product</a></li>
+                            </ul>
+                        </li>
                     </ul>
-            </aside>
-        </div>
+                </aside>
+            </div>
